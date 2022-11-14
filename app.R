3c8940b6fd7c5e20c67cfe2c067c09c9f75f65a4
ItsOyondi <97532392+ItsOyondi@users.noreply.github.com>
2022-11-14 08:46:28 -0500

    Upgraded the new GUI to work with helpers file

--- app.R
+++ app.R
@@ -8,60 +8,99 @@ library("wordcloud")
 library("RColorBrewer")
 library("ggwordcloud")
 library("ggplot2")
+library(fresh)
 source("helpers.R")
+
+custom_colors_theme <- create_theme(
+  theme = "default",
+  bs_vars_navbar(
+    default_bg = "#064273",
+    default_color = "#FFFFFF",
+    default_link_color = "#FFFFFF",
+    default_link_active_color = "#75b8d1",
+    default_link_active_bg = "#FFFFFF",
+    default_link_hover_color = "#aaa"
+  )
+)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Word Cloud Builder App"),
-  
-  #create an input field to users to select and upload files
-  sidebarLayout(
-    
-    # creating a side bar for selecting files
-    sidebarPanel(
-      
-      #create upload input field
-      fileInput("file1", "Select File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      
-      #separator
-      hr(),
-      
-      checkboxInput("showraw", label = "Show original text", value = TRUE),
-      
-      radioButtons("radio", label = h3("Available Options"),
-                   choices = list("Table" = 1, "Word Cloud" = 2), 
-                   selected = 1)
-      
+  navbarPage(
+    title = "WORD CLOUD BUILDER",
+    tags$style(type = 'text/css', '.navbar {
+                           font-family: Century Gothic;
+                           font-size: 13px;}',
+               
+               '.navbar-dropdown { 
+                           font-family: Century Gothic;
+                           font-size: 13px;}'
+               
     ),
-    
-    #a section for displaying the table with words extracted from the uploaded file
-    mainPanel(
-      
-      conditionalPanel(
-        "input.showraw == true", 
-        wellPanel(
-          textOutput("rawtext")
-        )
-      ),
-      
-      #output contents displayed by the table
-      conditionalPanel(
-        "input.radio == 1", 
-        tableOutput("table")
-      ), 
-      conditionalPanel(
-        "input.radio == 2", 
-        plotOutput("wdPlot")
-      ),
-      
-      
-    )
+    header = tagList(
+      use_theme(custom_colors_theme) #add background color
+    ),
+     tabPanel("Preprocessing",
+              # Application title
+              titlePanel(h1("Word Cloud Builder App", align = "center", style = "font-family: 'Century Gothic', serif;
+    font-weight: 500; font-size: 50px; text-shadow: 3px 3px 3px #aaa; line-height: 1; 
+     color: #404040;")),
+              
+              #create an input field to users to select and upload files
+              sidebarLayout(
+                
+                # creating a side bar for selecting files
+                sidebarPanel(
+                  
+                  #create upload input field
+                  fileInput("file1", "Select File",
+                            multiple = TRUE,
+                            accept = c("text/csv",
+                                       "text/comma-separated-values,text/plain",
+                                       ".csv")),
+                  
+                  #separator
+                  hr(),
+                  
+                  checkboxInput("showraw", label = "Show original text", value = TRUE),
+                  radioButtons("radio", label = h3("Available Options"),
+                               choices = list("Table" = 1, "Word Cloud" = 2), 
+                               selected = 1)
+                  
+                ),
+                
+                #a section for displaying the table with words extracted from the uploaded file
+                mainPanel(
+                  
+                
+                  conditionalPanel(
+                    "input.showraw == true", 
+                    wellPanel(
+                      textOutput("rawtext")
+                    )
+                  ),
+                  
+                  #output contents displayed by the table
+                  conditionalPanel(
+                    "input.radio == 1", 
+                    shiny::dataTableOutput("contents"),
+                  ), 
+                  conditionalPanel(
+                    "input.radio == 2", 
+                    plotOutput("wdPlot")
+                  ),
+                  
+                  
+                )
+              )
+     ),
+     navbarMenu("More Options",
+                tabPanel("Table",
+                         shiny::dataTableOutput("table")
+                )
+     )
   )
+  
+  
 )
 
 # Define server logic
@@ -80,28 +119,34 @@ server <- function(input, output) {
     df
   })
   
-  output$table <- renderTable({
+  output$contents <- shiny::renderDataTable({
     freq_dat()
   })
   
   output$wdPlot <- renderPlot({
     ggplot(freq_dat(), size=1.6, shape = 'diamond', aes(label = word, size=freq,
-                                                color = factor(sample.int(10, nrow(freq_dat()), replace = TRUE))
+                                                        color = factor(sample.int(10, nrow(freq_dat()), replace = TRUE))
     )) +
       geom_text_wordcloud() +scale_size_area(max_size = 16) +
       theme_minimal()
   })
-  
-  #section for displaying the story in textual format
-  output$rawtext <- renderText({
     
-    if(is.null(input$file1)){
-      return(d_story) #default story
+    #data table
+    output$table <- shiny::renderDataTable({
+      freq_dat()
+    })
+    #section for displaying the story in textual format
+    output$rawtext <- renderText({
       
-    }else{
-      return(input$file1$datapath) #display text from uploaded file
-    }
-  })
+      if(is.null(input$file1)){
+        return(d_story) #default story
+        
+      }else{
+        return(readLines(input$file1$datapath)) #display text from uploaded file
+      }
+    })
+  
+  
   
 }
 
